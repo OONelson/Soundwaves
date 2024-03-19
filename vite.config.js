0bb412788bc119c735ce04beed64874b89c692e3
Nelson <114698224+OONelson@users.noreply.github.com>
2024-03-19 23:02:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/Soundwaves/',
 	plugins: [vue()],
 	// resolve: {
 	// 	alias: {
